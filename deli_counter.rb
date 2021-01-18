b0bff292a3ac8a1a0c5112ccdb3166c56c207649
Elliott Vargo <vargoelliott@gmail.com>
2021-01-18 01:45:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   elsif katz_deli.size > 0
-    #print "The line is currently:" these two lines failed the tests due to the use of print in place of puts it seems
+
     katz_line = []
     katz_deli.each_with_index {|element, i| katz_line << " #{i+1}. #{element} "}
     puts ""
