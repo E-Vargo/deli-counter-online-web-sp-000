9e376b77fb61040488b594d40d8c8ed62b21f994
Elliott Vargo <vargoelliott@gmail.com>
2021-01-18 02:14:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def take_a_number(katz_deli, person)
   katz_deli.push(person)
-  puts "Welcome, #{person}. You are number #{katz_Deli.length} in line."
+  puts "Welcome, #{person}. You are number #{katz_deli.length} in line."
 end
 
 
