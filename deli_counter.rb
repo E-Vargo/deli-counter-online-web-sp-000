70c35dbd3ff117c6e004a3927734730e49119966
Elliott Vargo <vargoelliott@gmail.com>
2021-01-18 01:24:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   elsif katz_deli.size > 0
+    print "The line is currently:"
     katz_deli.each_with_index {|element, i| print " #{i+1}. #{element} "}
 
   end
