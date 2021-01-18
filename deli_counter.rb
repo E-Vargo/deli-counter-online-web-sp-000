990bdef208e46a04c2a57ae8a2e99ff9dd27eebe
Elliott Vargo <vargoelliott@gmail.com>
2021-01-18 01:49:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif katz_deli.size > 0
 
-    katz_line = []
+    katz_line = [] #creates new array to shovel iterated elements into and puts final
     katz_deli.each_with_index {|element, i| katz_line << " #{i+1}. #{element}"}
     puts "The line is currently:#{katz_line.join}"
 
