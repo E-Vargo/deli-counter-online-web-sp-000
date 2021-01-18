0bc55861352068ae0c6f6b725376c037885b3644
Elliott Vargo <vargoelliott@gmail.com>
2021-01-18 01:09:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,3 +6,12 @@ def line(katz_deli)
   elsif katz_deli.size > 0
 
 end
+
+def now_serving(katz_deli)
+    if katz_deli.size == 0
+      puts "There is nobody waiting to be served!"
+    elsif katz_deli.size > 0
+puts "Currently serving #{katz_deli[0]}."
+katz_deli.pop
+  end
+end
